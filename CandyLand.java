e615e480bbc7f66b35bd21dbef1ebdedbe1ca327
Zoe Fingleton <141530755+zoefing@users.noreply.github.com>
2024-04-22 21:06:11 -0400

    second major commit
    changes to code:
    - switched from java.util.Random to java.util.concurrent.ThreadLocalRandom which now handles normal card/dice behaviour (interval from 1-x instead of 0-x)
    - changed order of gingerbread man and candy cane man to reflect game lore
    - added end game! catch in drawCard() to make sure location doesn't go over 100, and catch in game loop to end game
    - added placeholder repercussions to losing boss battle
    - added typo catcher to boss battles... it only seems to work once for some reason but that can be resolved later

--- CandyLand.java
+++ CandyLand.java
@@ -1,5 +1,5 @@
-import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class CandyLand {
 
@@ -8,145 +8,211 @@ public class CandyLand {
     int life = 20;
     int points = 0;
     int location = 0;
-    boolean candyCaneManDefeated = false;
     boolean gingerbreadManDefeated = false;
+    boolean candyCaneManDefeated = false;
 
-//draw card function
-public void drawCard() {
+    // methods
 
-    Random rand = new Random();
-    int cardNum = rand.nextInt(10);
-    //needs to be greater than 1
-    System.out.println("You drew a: " + cardNum);
-    location += cardNum;
-    System.out.println("You are currently at square " + location);
-    System.out.println("Input 'draw' to continue, input 'quit' to end game.");
-}
-
-public void candyCaneMan() {
-    // candycane man lore
-    // easy boss
-    Scanner sc = new Scanner(System.in);
-    Random rand = new Random();
-    // plot point for dice
-    int diceNum = rand.nextInt(6);
-    System.out.println("You rolled a " + diceNum);
-    // decide consequences for life and location
-    if (diceNum >= 2) {
-        candyCaneManDefeated = true;
-        System.out.println("You have vanquished the Candy Cane Man");
-        // art?
-        System.out.println("You have gained a candycane (1)");
-        // could build inventory system
-        System.out.println("What would you like to do with this candycane");
-        // show options
-        System.out.println("Type 'life' to gain +2 health or type 'move' to advance +10 squares");
-        // leaderboard could be fun!
-        String userInput = sc.nextLine();
-        if (userInput.equals("life")) {
-            life += 2;
-            System.out.println("New life: " + life);
-        }
-        else if (userInput.equals("move")) {
-            location += 10;
-            System.out.println("New location: "+ location);
+    //draw card function
+    public void drawCard() {
+        // draws card in interval
+        int cardNum = ThreadLocalRandom.current().nextInt(1, 10);
+        System.out.println("You drew a: " + cardNum);
+        location += cardNum;
+        if (location >= 100) {
+            location = 100;
         }
         else {
-            System.out.println("Please type 'life' or 'move'");
+            System.out.println("You are currently at square " + location);
+            System.out.println("Input 'draw' to continue, input 'quit' to end game.");
         }
     }
-    else {
-        System.out.println("You failed to defeat the gingerbread man.");
-    }
 
-}
-
-public void gingerbreadMan() {
-    // gingerbread man
-    // medium level boss
-    Scanner sc = new Scanner(System.in);
-    Random rand = new Random();
-    int diceNum = rand.nextInt(6);
-    System.out.println("You rolled a " + diceNum);
-    // decide consequences for life and location
-    if (diceNum >= 4) {
-        gingerbreadManDefeated = true;
-        System.out.println("You beat the gingerbread man! \n You earned a gingerbread cookie (1). \n What would you like to do with this cookie? \n Type 'life' to gain +5 health or type 'move to advance 15 squares.");
-        String userInput = sc.nextLine();
-        if (userInput.equals("life")) {
-            life += 5;
-            System.out.println("New life: " + life);
+    // gingerbread man boss
+    // first boss, easy
+    public void gingerbreadMan() {
+        // create scanner
+        Scanner sc = new Scanner(System.in);
+        // set dice num as random number between 1 and 6
+        int diceNum = ThreadLocalRandom.current().nextInt(1, 7);
+        System.out.println("You rolled a " + diceNum);
+        // if dice roll sufficient, player wins
+        if (diceNum >= 2) {
+            // set boolean to true
+            gingerbreadManDefeated = true;
+            // offer player options for reward
+            System.out.println("You beat the Gingerbread Man! \nYou earned a gingerbread cookie (1). \nWhat would you like to do with this cookie? \nType 'life' to gain +2 health or type 'move' to advance 10 squares.");
+            String userInput = sc.nextLine();
+            // if player chooses life, add 2 to life
+            if (userInput.equals("life")) {
+                life += 2;
+                System.out.println("New life: " + life);
+            }
+            // if player chooses move, add 10 to location
+            else if (userInput.equals("move")) {
+                location += 10;
+                System.out.println("New location: "+ location);
+            }
+            // typo catcher
+            else {
+                System.out.println("Please type 'life' or 'move'");
+            }
         }
-        else if (userInput.equals("move")) {
-            location += 15;
-            System.out.println("New location: "+ location);
-        }
+        // if dice roll insufficient, player loses
         else {
-            System.out.println("Please type 'life' or 'move'");
+            System.out.println("You failed to defeat the gingerbread man.");
+            // randomly decide repercussions - this is subject to change
+            int repercussions = ThreadLocalRandom.current().nextInt(1, 2);
+            if (repercussions == 1) {
+                life -= 1; // subject to change, placeholder
+            }
+            if (repercussions == 2) {
+                location -= 5; // subject to change, placeholder
+            }
         }
     }
-    else {
-        System.out.println("You failed to defeat the gingerbread man.");
+
+    // candy cane man boss
+    // meidum-easy boss
+    public void candyCaneMan() {
+        // create scanner
+        Scanner sc = new Scanner(System.in);
+        // set dice num as random number between 1 and 6
+        int diceNum = ThreadLocalRandom.current().nextInt(1, 7);
+        System.out.println("You rolled a " + diceNum);
+        // if dice roll sufficient, player wins
+        if (diceNum >= 3) {
+            // set boolean to true
+            candyCaneManDefeated = true;
+            // offer player options for reward
+            System.out.println("You beat the Candy Cane Man! \nYou earned a candy cane (1). \nWhat would you like to do with this candy cane? \nType 'life' to gain +5 health or type 'move' to advance 15 squares.");
+            String userInput = sc.nextLine();
+            // if player chooses life, add 5 to life
+            if (userInput.equals("life")) {
+                life += 5;
+                System.out.println("New life: " + life);
+            }
+            // if player chooses move, add 15 to location
+            else if (userInput.equals("move")) {
+                location += 15;
+                System.out.println("New location: "+ location);
+            }
+            // typo catcher
+            else {
+                System.out.println("Please type 'life' or 'move'");
+            }
+        }
+        // if dice roll insufficient, player loses
+        else {
+            System.out.println("You failed to defeat the candy cane man.");
+            // randomly decide repercussions - this is subject to change
+            int repercussions = ThreadLocalRandom.current().nextInt(1, 2);
+            if (repercussions == 1) {
+                life -= 4; // subject to change, placeholder
+            }
+            if (repercussions == 2) {
+                location -= 10; // subject to change, placeholder
+            }
+        }
     }
 
-}
+    //main
+    public static void main(String[] args) {
+        // initialize game
+        boolean playing = true;
+        System.out.println("Welcome to CandyLand!");
+        // create game object
+        CandyLand game = new CandyLand();
+        // create scanner
+        Scanner sc = new Scanner(System.in);
+        // turn message w/ instructions
+        System.out.println("Input 'draw' to draw a card, input 'quit' to end game.");
 
-//main
-public static void main(String[] args) {
-    //if you're within a certain interval of squares, you get the choice to engage with bosses (10 squares)
-    boolean playing = true;
-    CandyLand game = new CandyLand();
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Input 'draw' to continue, input 'quit' to end game.");
-    // weird gap
+        // main game loop
+        while (playing) {
+            // check if player in end game
+            if (game.location == 100) {
+                // initalize end game
+                System.out.println("You are at the end of the board!");
+                System.out.println("Game over!");
+                // ideally, print achievements here
+                // i.e., you beat x bosses, you took x time, you lost x life
+                break;
+            }
 
-    System.out.println();
-    while (playing) {
-        String userInput = sc.nextLine();
+            String userInput = sc.nextLine();
 
-        // if statements for each boss
-
-        if (userInput.equals("draw")) {
-            game.drawCard();
-            if (game.location >= 10 && game.location <= 20) {
-                if (game.candyCaneManDefeated != true) {
-                    System.out.println("You are in the teritory of the Candy Cane Man");
-                    System.out.println("Would like to fight? Type 'yes' or 'no' to make your decision.");
-                    String fightQuestion = sc.nextLine();
-    
-                    if (fightQuestion.equals("yes")) {
-                        game.candyCaneMan();
-                        System.out.println("You continue on your way.");
-                        System.out.println("Input 'draw' to continue, input 'quit' to end game.");
-                    }
-                    else if (fightQuestion.equals("no")) {
-                        System.out.println("You choose not to battle.");
-                        game.drawCard();
+            // main game loop
+            if (userInput.equals("draw")) {
+                // draw card
+                game.drawCard();
+                // if statements for each boss
+                // gingerbread man
+                if (game.location >= 10 && game.location <= 20) {
+                    // check if boss defeated
+                    // if not, prompt player to fight
+                    if (game.gingerbreadManDefeated != true) {
+                        // offer player option to fight
+                        System.out.println("You are in the territory of the Gingerbread man! \nWould you like to fight? Type 'yes' or 'no' to make your decision.");
+                        String fightQuestion = sc.nextLine();
+                        // if player chooses to fight, call gingerbreadMan method
+                        if (fightQuestion.equals("yes")) {
+                            game.gingerbreadMan();
+                            System.out.println("You continue on your way. \nInput 'draw' to continue, input 'quit' to end game.");
+                        }
+                        // if player chooses not to fight, continue game
+                        else if (fightQuestion.equals("no")) {
+                            System.out.println("You choose not to battle.");
+                            game.drawCard();
+                        }
+                        // typo catcher
+                        // note: only works once for some reason?
+                        else {
+                            System.out.println("Please type either 'yes' or 'no' to make your decision.");
+                        }
                     }
                 }
-            }
-            if (game.location >= 30 && game.location <= 40) {
-                if (game.gingerbreadManDefeated != true) {
-                    System.out.println("You are in the territory of the Gingerbread man! \n Would you like to fight? Type 'yes' or 'no' to make your decision.");
-                    String fightQuestion = sc.nextLine();
 
-                    if (fightQuestion.equals("yes")) {
-                        game.gingerbreadMan();
-                        System.out.println("You continue on your way. \n Input 'draw' to continue, input 'quit' to end game.");
+                // candy cane man
+                if (game.location >= 30 && game.location <= 40) {
+                    // check if boss defeated
+                    // if not, prompt player to fight
+                    if (game.candyCaneManDefeated != true) {
+                        // offer player option to fight
+                        System.out.println("You are in the teritory of the Candy Cane Man \nWould like to fight? Type 'yes' or 'no' to make your decision.");
+                        String fightQuestion = sc.nextLine();
+                        // if player chooses to fight, call candyCaneMan method
+                        if (fightQuestion.equals("yes")) {
+                            game.candyCaneMan();
+                            System.out.println("You continue on your way. \nInput 'draw' to continue, input 'quit' to end game.");
+                        }
+                        // if player chooses not to fight, continue game
+                        else if (fightQuestion.equals("no")) {
+                            System.out.println("You choose not to battle.");
+                            game.drawCard();
+                        }
+                        // typo catcher
+                        // note: only works once for some reason?
+                        else {
+                            System.out.println("Please type either 'yes' or 'no' to make your decision.");
+                        }
                     }
-                }
+                }     
+            
             }
-        }     
-        
-        else if (userInput.equals("quit")) {
-            System.out.println("Game over");
-            break;
-        }
-
-        else {
-            System.out.println("Please type either 'draw' or 'quit' to continue.");
-        }
+            
+            // game quit option
+            else if (userInput.equals("quit")) {
+                System.out.println("Game over");
+                break;
+            }
+            
+            // typo catcher
+            else {
+                System.out.println("Please type either 'draw' or 'quit' to continue.");
+            }
+        } 
+        // close scanner
+        sc.close();
     }
-    
-} 
 }
